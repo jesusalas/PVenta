7a54b74da325e2e89b4143de88660ca92f93d557
Jesus <jesusalas18@gmail.com>
2019-09-02 15:32:02 -0600

    (test): se agrego proyecto test con cambios en interfax grafica

---
+++ PVenta/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.saleDetailDataGridView = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.clientComboBox = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.saleDetailDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Codigo"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(319, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Agregar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(25, 90)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'saleDetailDataGridView
+        '
+        Me.saleDetailDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.saleDetailDataGridView.Location = New System.Drawing.Point(25, 131)
+        Me.saleDetailDataGridView.Name = "saleDetailDataGridView"
+        Me.saleDetailDataGridView.Size = New System.Drawing.Size(369, 150)
+        Me.saleDetailDataGridView.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(146, 90)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(143, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Cantidad"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Cliente"
+        '
+        'clientComboBox
+        '
+        Me.clientComboBox.FormattingEnabled = True
+        Me.clientComboBox.Location = New System.Drawing.Point(100, 26)
+        Me.clientComboBox.Name = "clientComboBox"
+        Me.clientComboBox.Size = New System.Drawing.Size(294, 21)
+        Me.clientComboBox.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(25, 309)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Guardar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(108, 309)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Buscar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(198, 309)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Cancelar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.clientComboBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.saleDetailDataGridView)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.saleDetailDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents saleDetailDataGridView As DataGridView
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents clientComboBox As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ PVenta/Form1.vb
@@ -0,0 +1,128 @@
+﻿
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim conexion As New SqlClient.SqlConnection
+    Dim comando As New SqlClient.SqlCommand
+    Dim adaptador As New SqlClient.SqlDataAdapter
+    Dim resultado As SqlClient.SqlDataReader = Nothing
+    Dim misDatos As New DataSet
+    Public conectar As New SqlClient.SqlConnection
+
+    Dim dataAdapter As SqlDataAdapter
+    Dim dataReader As SqlDataReader
+    Dim dataSet As New DataSet
+
+    Dim connectionString As String = "server=172.16.1.109; user id=sa; password=Dsdsistemas2012; database=dsd_mazatlan;"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ClientsLoad()
+    End Sub
+
+    Private Function consulta(con As String)
+        conexion.Close()
+        conexion.ConnectionString = connectionString
+        Try
+            If (abrir()) Then
+                comando.CommandText = con
+                comando.Connection = conexion
+                comando.CommandTimeout = 0
+                resultado = comando.ExecuteReader()
+            End If
+        Catch ex As SqlClient.SqlException
+            MessageBox.Show("Error al realizar la funcion Consulta. " + ex.Message, "Sistema DSD", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Return resultado
+    End Function
+
+    Private Function abrir()
+        Dim exito As Boolean = False
+        Try
+            If conexion.State = ConnectionState.Closed Then
+                conexion.Open()
+                exito = True
+            End If
+        Catch ex As SqlClient.SqlException
+            MessageBox.Show("Error al conectar al Servidor. " + ex.Message, "Sistema DSD", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Return exito
+    End Function
+
+    Public Function cerrar()
+        Dim exito As Boolean = False
+        Try
+            If conexion.State = ConnectionState.Open Then
+                conexion.Close()
+                exito = True
+            End If
+        Catch ex As SqlClient.SqlException
+            MessageBox.Show("Error al desconectar al Servidor. " + ex.Message, "Sistema DSD", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Return exito
+    End Function
+
+    Private Function llenar_tabla(con As String)
+        conexion.ConnectionString = connectionString
+        Try
+            If (abrir()) Then
+                comando.CommandText = con
+                comando.Connection = conexion
+                comando.CommandTimeout = 0
+                adaptador.SelectCommand = comando
+            End If
+        Catch ex As SqlClient.SqlException
+            MessageBox.Show("Error al realizar la funcion llenar_tabla. " + ex.Message, "Sistema DSD", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Return adaptador
+    End Function
+
+    Private Sub ClientsLoad()
+
+        If dataSet.Tables.Contains("cat_productos") Then dataSet.Tables("cat_productos").Clear()
+
+        dataAdapter = llenar_tabla("SELECT TOP(10) c.id_producto, c.nombre FROM cat_productos c WHERE c.activo = 1 ORDER BY c.id_grupo_proveedor, c.id_linea")
+        dataAdapter.Fill(dataSet, "cat_productos")
+        cerrar()
+
+        clientComboBox.DataSource = dataSet.Tables("cat_productos")
+        'clientComboBox.DataSource = DataHelper.LoadDataTable
+        clientComboBox.DisplayMember = "nombre"
+        clientComboBox.ValueMember = "id_producto"
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Chr(13) Then
+
+            Dim query As String
+            Dim hasProduct As Boolean
+
+            query = "SELECT nombre FROM cat_productos WHERE id_producto = " & TextBox1.Text.Trim
+
+            dataReader = consulta(query)
+            If dataReader.HasRows Then
+                If dataReader.Read Then
+                    MsgBox(dataReader("nombre"))
+                End If
+            Else
+                MsgBox("no se encontro producto")
+            End If
+            cerrar()
+
+        End If
+    End Sub
+End Class
